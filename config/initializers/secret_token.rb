3ab24c13dba942ab9c7da69c08f93d93851d8b96
Ken Kendall <kendall@okcupidlabs.com>
2012-10-26 22:42:47 -0700

    :open_hands: generate rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anotheritcompany::Application.config.secret_token = 'e01b3c40e0b27e2a62e888dc52f76ab929304d08a70d2908ea1fe02d3c889f132da47dd5929419dfb99bfa5900b92fc52f2d25b387057b842566041e3c5a3f67'
