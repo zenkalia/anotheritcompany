d6a5be9bc432938e3d9191d8c2c38496aaf8a890
Ken Kendall <kendall@okcupidlabs.com>
2013-01-06 13:27:16 -0800

    :key: removing user model because devise is going to generate it for me

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem 'twitter-bootstrap-rails'
 gem 'therubyracer'
 gem 'less-rails'
 
+gem 'devise'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130105082405) do
-
-  create_table "users", :force => true do |t|
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "last_login"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-  end
+ActiveRecord::Schema.define(:version => 0) do
 
 end
