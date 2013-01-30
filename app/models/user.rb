e0e4ef4d7b1f41bcde2dc082570b5b8b9c891f6b
Ken Kendall <kendall@okcupidlabs.com>
2013-01-29 21:44:40 -0800

    :herb: fields must be attr_accessible to be editable in rails_admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :admin
+  attr_accessible :email,
+                  :password,
+                  :password_confirmation,
+                  :remember_me,
+                  :admin,
+                  :kaseya_link,
+                  :kaseya_download
   # attr_accessible :title, :body
 end
